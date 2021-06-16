cb8209d403670c6c1baa387f3f7a027a63a169fa
rebekkaschaub <re.schaub@web.de>
2021-06-16 17:46:32 +0200

    Add link to detailsPage

--- frontend/src/components/CounselingCenterCard.js
+++ frontend/src/components/CounselingCenterCard.js
@@ -1,24 +1,28 @@
 import styled from "styled-components/macro";
+import { Link } from "react-router-dom";
 
 export default function CounselingCenterCard({ counselingCenter }) {
   return (
-    <Wrapper>
-      <h2>{counselingCenter.name}</h2>
-    </Wrapper>
+    <Link to={`/counseling/${counselingCenter.id}/details`}>
+      <Wrapper>
+        <h3>{counselingCenter.name}</h3>
+      </Wrapper>
+    </Link>
   );
 }
 
 const Wrapper = styled.div`
   padding: 4px;
-  margin: 10px 5px;
+  margin: 10px 0;
   transition: 0.3s;
-  box-shadow: 0px 8px 5px -5px rgba(0, 0, 0, 0.1);
-  font-size: 12px;
+  box-shadow: -2px -2px 5px 2px rgba(0, 0, 0, 0.1);
+  font-size: small;
   background-color: #ffff;
   border-radius: 12px;
+  text-decoration: none;
 
   &:hover {
-    box-shadow: 0px 15px 9px -7px rgba(0, 0, 0, 0.1);
+    box-shadow: 0 15px 9px -7px rgba(0, 0, 0, 0.1);
     transform: scale(1.01, 1.01);
   }
 `;
