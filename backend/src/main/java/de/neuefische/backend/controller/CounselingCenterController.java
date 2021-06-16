02410d917fd5bd6366f56ff0cfb4362c2b65da08
rebekkaschaub <re.schaub@web.de>
2021-06-16 17:43:56 +0200

    Add endpoint getbyid to backend

--- backend/src/main/java/de/neuefische/backend/controller/CounselingCenterController.java
+++ backend/src/main/java/de/neuefische/backend/controller/CounselingCenterController.java
@@ -3,11 +3,15 @@ package de.neuefische.backend.controller;
 import de.neuefische.backend.model.CounselingCenter;
 import de.neuefische.backend.service.CounselingCenterService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("api/counseling")
@@ -24,6 +28,19 @@ public class CounselingCenterController {
     public List<CounselingCenter> listAllCounselingCenters(){
 
         return service.listAllCounselingCenters();
+
+    }
+
+    @GetMapping("/{id}")
+    public CounselingCenter getCounselingCenterById(@PathVariable String id){
+
+        Optional<CounselingCenter> optionalCounselingCenter = service.getCounselingCenterById(id);
+
+        if(optionalCounselingCenter.isEmpty()){
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "id does not exist");
+        }
+
+        return optionalCounselingCenter.get();
 
     }
 }
--- backend/src/main/java/de/neuefische/backend/service/CounselingCenterService.java
+++ backend/src/main/java/de/neuefische/backend/service/CounselingCenterService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CounselingCenterService {
@@ -19,5 +20,9 @@ public class CounselingCenterService {
 
     public List<CounselingCenter> listAllCounselingCenters() {
         return repo.findAll();
+    }
+
+    public Optional<CounselingCenter> getCounselingCenterById(String id) {
+        return repo.findById(id);
     }
 }
