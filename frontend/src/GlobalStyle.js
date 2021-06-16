460952e0c0e10dc291fadc43bc95c9eba0c3637c
rebekkaschaub <re.schaub@web.de>
2021-06-16 09:19:34 +0200

    Add GlobalStyle

---
+++ frontend/src/GlobalStyle.js
@@ -0,0 +1,13 @@
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle`
+    *{
+      box-sizing: border-box;
+    }
+    
+    html, body{
+      margin: 0;
+      font-family: "Roboto",sans-serif;
+      color: #1c3648;
+    }
+`;
