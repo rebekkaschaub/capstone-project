339cd518b2a75db8dc00fe3aeccdd0079ce63373
rebekkaschaub <re.schaub@web.de>
2021-06-16 12:42:01 +0200

    Add DB

--- backend/src/main/java/de/neuefische/backend/repos/CounselingCenterRepo.java
+++ backend/src/main/java/de/neuefische/backend/repos/CounselingCenterRepo.java
@@ -1,38 +1,13 @@
 package de.neuefische.backend.repos;
 
-import de.neuefische.backend.model.Address;
 import de.neuefische.backend.model.CounselingCenter;
-import de.neuefische.backend.model.CounselingSetting;
-import de.neuefische.backend.model.TargetGroup;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Repository
-public class CounselingCenterRepo {
+public interface CounselingCenterRepo extends PagingAndSortingRepository<CounselingCenter, String> {
 
-    private final List<CounselingCenter> counselingCenters = new ArrayList<>(List.of(
-            CounselingCenter.builder()
-                .name("SEEHAUS Suchtberatungs- und Behandlungszentrum Wandsbek")
-                .address(Address.builder().street("Hasselbrookstraße 94a").postalCode("22089").city("Hamburg").build())
-                .phoneNo("040 2000102000")
-                .email("info@seehaus-hh.de")
-                .url("http://www.therapiehilfe.de")
-                .specializations(List.of("Suchtberatung", "Vermittlung von Selbsthilfegruppen", "Gruppenarbeit", "Krisenintervention"))
-                .targetGroup(List.of(TargetGroup.INDIVIDUAL))
-                .counselingSetting(List.of(CounselingSetting.INPERSON, CounselingSetting.PHONE)).build(),
-            CounselingCenter.builder()
-                .name("Erziehungsberatungsstelle Billstedt")
-                .address(Address.builder().street("Öjendorfer Weg 10a").postalCode("22111").city("Hamburg").build())
-                .phoneNo("040 280140-620")
-                .email("erziehungsberatung@caritas-hamburg.de")
-                    .url("http://www.caritas-hamburg.de ")
-                    .specializations(List.of("Erziehungsberatung", "Beratung für Kinder", "Jugendliche und Eltern (einschl. Beratung bei Trennung und Scheidung)" ))
-                    .targetGroup(List.of(TargetGroup.INDIVIDUAL, TargetGroup.RELATIVES))
-                    .counselingSetting(List.of(CounselingSetting.INPERSON, CounselingSetting.PHONE)).build()));
-
-    public List<CounselingCenter> findAll() {
-        return counselingCenters;
-    }
+    List<CounselingCenter> findAll();
 }
