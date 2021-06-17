0d8198009b42027d6095006565143312dc6be057
rebekkaschaub <re.schaub@web.de>
2021-06-17 09:24:02 +0200

    Remove unused import

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,6 @@
 import ResultsListPage from "./pages/ResultsListPage";
 import { Switch, Route } from "react-router-dom";
 
-import DetailsPage from "./pages/DetailsPage";
 import Header from "./components/Header";
 
 export default function App() {
