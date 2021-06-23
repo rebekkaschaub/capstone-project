2da06dac307fbd34d23580c4399938df4cdbbb3b
rebekkaschaub <re.schaub@web.de>
2021-06-23 11:43:08 +0200

    Add new Route to SearchPage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import { Switch, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
 import DetailsPage from "./pages/DetailsPage";
 import Layout from "./GlobalLayout/Layout";
+import FilterPage from "./pages/FilterPage";
 
 export default function App() {
   return (
@@ -11,6 +12,7 @@ export default function App() {
         <Switch>
           <Route component={LandingPage} path="/" exact />
           <Route component={ResultsListPage} path="/counseling" exact />
+          <Route component={FilterPage} path="/search" exact />
           <Route component={DetailsPage} path="/counseling/:id/details" exact />
         </Switch>
       </Layout>
