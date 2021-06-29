dedfa4a93d43f373ca50484a21d03e6f78c95880
rebekkaschaub <re.schaub@web.de>
2021-06-29 13:01:28 +0200

    Add securityConfig

---
+++ backend/src/main/java/de/neuefische/backend/security/config/SecurityConfig.java
@@ -0,0 +1,40 @@
+package de.neuefische.backend.security.config;
+
+import de.neuefische.backend.security.service.AppUserDetailsService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final AppUserDetailsService appUserDetailsService;
+
+    public SecurityConfig(AppUserDetailsService appUserDetailsService) {
+        this.appUserDetailsService = appUserDetailsService;
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(appUserDetailsService);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable().authorizeRequests()
+                .antMatchers("/api/counseling").permitAll()
+                .antMatchers("/auth/login").permitAll()
+                .antMatchers("/api/**").authenticated()
+                .and().formLogin()
+                .and().httpBasic();
+    }
+}
