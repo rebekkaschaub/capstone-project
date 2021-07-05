682da0fbb8840db71bc4e63eafc7799817d41d36
rebekkaschaub <re.schaub@web.de>
2021-07-05 16:28:59 +0200

    Add tests for delete method

--- backend/src/test/java/de/neuefische/backend/service/ReviewServiceTest.java
+++ backend/src/test/java/de/neuefische/backend/service/ReviewServiceTest.java
@@ -4,9 +4,13 @@ import de.neuefische.backend.dto.ReviewDto;
 import de.neuefische.backend.model.Review;
 import de.neuefische.backend.repos.ReviewRepo;
 import de.neuefische.backend.utils.IdUtils;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+import java.util.Optional;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
@@ -17,6 +21,11 @@ class ReviewServiceTest {
     private final ReviewRepo repo = mock(ReviewRepo.class);
     private final IdUtils idUtils = mock(IdUtils.class);
     private final ReviewService service = new ReviewService(repo, idUtils);
+
+    @BeforeEach
+    public void clearDb() {
+        repo.deleteAll();
+    }
 
     @Test
     @DisplayName("method addReview should add a new Review to db")
@@ -71,7 +80,79 @@ class ReviewServiceTest {
     }
 
     @Test
-    void deleteReview() {
+    @DisplayName("method deleteReview should delete review by id")
+    void deleteReviewWithMatchingUsername() {
+        repo.saveAll(List.of(Review.builder()
+                    .reviewId("42")
+                    .counselingCenterId("123")
+                    .counselingCenterName("Phobieberatung")
+                    .author("DerBizeps")
+                    .title("War gut")
+                    .rating(5)
+                    .comment("Mega! 5 Sterne")
+                    .build(),
+                Review.builder()
+                    .reviewId("90")
+                    .counselingCenterId("765")
+                    .counselingCenterName("Suchtberatung")
+                    .author("DerTrizeps")
+                    .title("Supi")
+                    .rating(4)
+                    .comment("Alles ok")
+                    .build()));
+
+        when(repo.findById("42")).thenReturn(Optional.of(Review.builder()
+                .reviewId("42")
+                .counselingCenterId("123")
+                .counselingCenterName("Phobieberatung")
+                .author("DerBizeps")
+                .title("War gut")
+                .rating(5)
+                .comment("Mega! 5 Sterne")
+                .build()));
+
+        //WHEN
+        service.deleteReview("42", "DerBizeps");
+
+        //THEN
+        verify(repo).deleteById("42");
+    }
+
+    @Test
+    @DisplayName("method deleteReview should throw HttpStatus Forbidden, when review with not matching username should be deleted")
+    void deleteReviewWithNotMatchingUsername() {
+        repo.saveAll(List.of(Review.builder()
+                        .reviewId("42")
+                        .counselingCenterId("123")
+                        .counselingCenterName("Phobieberatung")
+                        .author("DerBizeps")
+                        .title("War gut")
+                        .rating(5)
+                        .comment("Mega! 5 Sterne")
+                        .build(),
+                Review.builder()
+                        .reviewId("90")
+                        .counselingCenterId("765")
+                        .counselingCenterName("Suchtberatung")
+                        .author("DerTrizeps")
+                        .title("Supi")
+                        .rating(4)
+                        .comment("Alles ok")
+                        .build()));
+
+        when(repo.findById("42")).thenReturn(Optional.of(Review.builder()
+                .reviewId("42")
+                .counselingCenterId("123")
+                .counselingCenterName("Phobieberatung")
+                .author("DerBizeps")
+                .title("War gut")
+                .rating(5)
+                .comment("Mega! 5 Sterne")
+                .build()));
+
+        //WHEN/THEN
+        assertThrows(ResponseStatusException.class, ()->  service.deleteReview("42", "DerTrizeps"));
+        verify(repo, never()).deleteById("42");
     }
 
     @Test
